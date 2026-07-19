fd43c53b522a6906c9b389088a18224ccd69eb5a
ricardopadua <ricardo.padua4@gmail.com>
2026-07-19 12:14:24 -0300

    initial commit

---
+++ lib/lineup.ex
@@ -0,0 +1,18 @@
+defmodule Lineup do
+  @moduledoc """
+  Documentation for `Lineup`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Lineup.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lineup_test.exs
@@ -0,0 +1,8 @@
+defmodule LineupTest do
+  use ExUnit.Case
+  doctest Lineup
+
+  test "greets the world" do
+    assert Lineup.hello() == :world
+  end
+end
